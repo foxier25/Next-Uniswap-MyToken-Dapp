16900604d44492093918aaa994c590d086f87a10
emiz98 <64089619+emiz98@users.noreply.github.com>
2022-05-17 15:30:26 +0530

    Timeago Fix

--- utils/utils.js
+++ utils/utils.js
@@ -58,5 +58,5 @@ export const username = (address) => {
 
 export const date_time = (unixTime) => {
   const date = new Date(unixTime * 1000)
-  return date.toLocaleDateString('en-US')
+  return date
 }
